d9ef48339246b33588fd2e53c2e14b5611f731a3
Mirage <cyprien.2003@icloud.com>
2023-10-14 15:30:31 +0200

    build

---
+++ src/data/aboutCompetencesData.js
@@ -0,0 +1,13 @@
+
+
+export const aboutMeData = {
+	CompetencesHeader: {
+		title: "Mes Compétences"
+	},
+	CompetencesInfo: {
+		CompetencesBallisage: 'Langages de Ballisage :',
+		CompetencesStyle: 'Langages de Style :',
+		CompetencesProg: 'Langages de Programmation :',
+		CompetencesFrame: 'Framework :',
+	}
+};
